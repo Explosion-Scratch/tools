595a4ee740c6ae71229ce62c176321509de603af
Explosion-Scratch <explosion.scratch@gmail.com>
2022-04-22 17:21:43 +0000

    Maybe this time????

--- svelte.config.js
+++ svelte.config.js
@@ -7,13 +7,13 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		paths: { base: '/tools' },
 		adapter: adapter(),
 		prerender: { default: true },
 		vite: {
 			optimizeDeps: { exclude: ['codejar'] },
+			mode: 'production',
 			resolve: {
 				alias: {
 					$public: resolve('public'),
