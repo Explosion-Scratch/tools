72c4ac7777d5870b20f6643d1fe02f56827be361
Explosion-Scratch <explosion.scratch@gmail.com>
2022-04-22 17:25:43 +0000

    Please work this time

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -26,7 +26,17 @@
 <Nav />
 
 <slot />
-<svelte:head><base href="/tools/" /></svelte:head>
+<svelte:head>
+	<base href="/tools/" />
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<title>Tools</title>
+	<link
+		rel="icon"
+		type="image/svg+xml"
+		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22%232ec7ce%22></rect><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2276%22>😄</text></svg>"
+	/>
+</svelte:head>
 
 <style>
 	:global(html, body, .reset) {
